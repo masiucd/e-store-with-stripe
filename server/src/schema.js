d6477f150f36c12168fe97669e85267b4cfad9b5
marcell <ciszekmarcell@gmail.com>
2019-04-23 13:20:27 +0200

    auth

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,9 +2,40 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    launches: [Launch]!
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
   type Launch {
     id: ID!
@@ -14,7 +45,7 @@ const typeDefs = gql`
     isBooked: Boolean!
   }
   type Rocket {
-    id: Rocket!
+    id: ID!
     name: String
     type: String
   }
@@ -23,32 +54,14 @@ const typeDefs = gql`
     email: String!
     trips: [Launch]!
   }
-
   type Mission {
     name: String
     missionPatch(size: PatchSize): String
   }
-
   enum PatchSize {
     SMALL
     LARGE
   }
-  type Mutation {
-    # if false, booking trips failed -- check errors
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-    # if false, cancellation failed -- check errors
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-
-    login(email: String): String # login token
-  }
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
-  }
 `;
 
-//  launches: [Launch]! = array of launches , never be null
-// Queries for the current user
-//   me: User
 module.exports = typeDefs;
