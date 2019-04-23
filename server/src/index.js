9dcf7c1750f7a2d0701ef2dff7f6bea6bcaf75f1
marcell <ciszekmarcell@gmail.com>
2019-04-23 07:36:58 +0200

    graph ql schema

--- server/src/index.js
+++ server/src/index.js
@@ -2,3 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url} ...`);
+});
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,54 @@
 const { gql } = require('apollo-server');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  type Rocket {
+    id: Rocket!
+    name: String
+    type: String
+  }
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-module.exports typeDefs;
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+  }
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
+
+//  launches: [Launch]! = array of launches , never be null
+// Queries for the current user
+//   me: User
+module.exports = typeDefs;
