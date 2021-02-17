e920a7cbf4edaa479b5121a3da09261b40019cdb
Masiu <ciszekmarcell@gmail.com>
2021-02-17 18:40:59 +0100

    feat: stripe integration

--- lib/payment.ts
+++ lib/payment.ts
@@ -1,6 +1,9 @@
 import { loadStripe } from "@stripe/stripe-js"
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY || "")
+// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY || "")
+const stripePromise = loadStripe(
+  `pk_test_51IJMQbCYzbrCXwtl1ezU8lYb6Coqhnfheo3p5KRvWN0Y1h03ksaNKJwpX3tUwSbufE6gAQYI9z5HiPcwOpmHcRKG00UJgPrla5`
+)
 
 export async function initializeCheckout({ lineItems }: Record<any, any> = {}): Promise<void> {
   const stripe = await stripePromise
